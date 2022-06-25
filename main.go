716585ab3cadade9145c599c73cf23e4b8780722
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-06-25 15:37:06 +0300

    step 6: splitted app into main/handlers/render files

--- main.go
+++ main.go
@@ -2,30 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const appPort = ":8081";
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the home page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
-}
 
 // main in the main application function
 func main()  {
