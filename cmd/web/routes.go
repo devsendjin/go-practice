a3d419178f88ec5f70b7aa21514b59af339f69b3
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-06-29 03:41:04 +0300

    step 14: added "chi" router + Recoverer middleware, removed "pat" router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
 	"github.com/devsendjin/go-practice/pkg/config"
 	"github.com/devsendjin/go-practice/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
